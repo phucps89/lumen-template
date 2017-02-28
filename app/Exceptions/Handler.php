1ceae45247e588a1faf0e2558e499d909158869d
Phuc Tran <phuc.thanh.tran@seldatinc.com>
2017-02-28 09:41:44 +0700

    init project

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,11 +3,19 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -45,6 +53,28 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        switch (get_class($e)) {
+            case TokenExpiredException::class :
+                return \RS::send('Token is expired', Response::HTTP_UNAUTHORIZED, true);
+            case TokenInvalidException::class :
+                return \RS::send('Invalid token', Response::HTTP_UNAUTHORIZED, true);
+            case JWTException::class :
+                return \RS::send('Token not found', Response::HTTP_UNAUTHORIZED);
+            case UnauthorizedHttpException::class :
+                return \RS::send('Invalid token', Response::HTTP_UNAUTHORIZED, true);
+            case NotFoundHttpException::class :
+                return \RS::send('Route not found', Response::HTTP_NOT_FOUND);
+            case MethodNotAllowedHttpException::class :
+                return \RS::send('Method not allowed', Response::HTTP_METHOD_NOT_ALLOWED);
+            case BadRequestHttpException::class :
+                return \RS::send($e->getMessage(), Response::HTTP_BAD_REQUEST);
+            default :
+                return \RS::send([
+                    'message' => $e->getMessage(),
+                    'traces'  => $e->getTrace()
+                ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        //return parent::render($request, $e);
     }
 }

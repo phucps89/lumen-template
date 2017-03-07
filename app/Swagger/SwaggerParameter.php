aed5999d6ab147b3df67aab8ec0cd45853ae39f8
Phuc Tran <phuc.thanh.tran@seldatinc.com>
2017-03-07 08:34:34 +0700

    init project

---
+++ app/Swagger/SwaggerParameter.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *      @SWG\Parameter(
+ *         parameter="auth_header",
+ *         name="Authorization",
+ *         in="header",
+ *         description="Token",
+ *         required=true,
+ *         type="string",
+ *         default="Bearer "
+ *     ),
+ */

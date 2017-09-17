c170a86c39e044199cd10c1d46ffd6e359e4debb
phuctran <phuc.ps.89@gmail.com>
2017-09-17 16:58:30 +0700

    update 5.5

--- app/Swagger/SwaggerDefine.php
+++ app/Swagger/SwaggerDefine.php
@@ -11,3 +11,23 @@
  *   basePath="/"
  * )
  */
+
+/**
+ *      @SWG\Definition(
+ * 		    definition="response_template_data",
+ *          description="Response with data is array or object",
+ * 		    @SWG\Property(property="_type", type="string", description="type", default="success"),
+ * 		    @SWG\Property(property="_time", type="string", description="Time response", default="2016-05-19 02:43:52"),
+ * 		    @SWG\Property(property="results", type="array|object", description="Data response")
+ *      )
+ */
+
+/**
+ *      @SWG\Definition(
+ * 		    definition="response_template_message",
+ *          description="Response with data is message",
+ * 		    @SWG\Property(property="_type", type="string", description="type", default="success"),
+ * 		    @SWG\Property(property="_time", type="string", description="Time response", default="2016-05-19 02:43:52"),
+ * 		    @SWG\Property(property="message", type="string", description="Message from server", default="Message content")
+ *      )
+ */
